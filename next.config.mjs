b133244c2da381ffef3e63ff81148da628a813df
MoradAit <43802122+Wuradclan@users.noreply.github.com>
2026-03-19 06:50:19 +0000

    next.confiog to optimize  images to webp

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    // Utile pour passer le build si tu as des petites erreurs de types
+    ignoreBuildErrors: true, 
   },
   images: {
-    unoptimized: true,
+    // On active l'optimisation (donc on ne met PAS unoptimized: true)
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net',
+      },
+    ],
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
